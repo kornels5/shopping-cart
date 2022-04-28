86da5118a3c2bf4331c00051c63625bc65105500
Kornel <srokakornel@gmail.com>
2022-04-28 18:32:04 +0200

    Created boilerplate

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div className="text-lg font-medium">App comonent</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
